d8eb154493da6ba7fddf048c7f3f4d75f39bc2f4
nono112002 <nono@tokuiten.jp>
2024-04-20 14:37:11 +0900

    M5stackに載せ替え

--- temperture_sensor_to_ambient.ino
+++ temperture_sensor_to_ambient.ino
@@ -1,5 +1,6 @@
-//#include <M5Stack.h>
-#include <M5StickCPlus.h>
+#include <M5Stack.h>
+//#include <M5StickCPlus.h>
+//#include <M5AtomS3.h>
 #include <WiFi.h>
 #include <Ambient.h>
 //#include "Adafruit_Si7021.h"
@@ -8,7 +9,7 @@
 #define SENSER_BIT    9      // 精度の設定bit
 
   //温度表示確認用DS18B20用(1)
-#define ONE_WIRE_BUS 32 // データ(黄)で使用するポート番号
+#define ONE_WIRE_BUS 21 // データ(黄)で使用するポート番号
 
 OneWire oneWire(ONE_WIRE_BUS);//DS18B20用
 DallasTemperature sensorsds(&oneWire);//温度表示確認用DS18B20用
@@ -24,58 +25,56 @@ const char* password = "tt2023wfp116";
 WiFiClient client;
 Ambient ambient;
 
-unsigned int channelId = 45848;
-const char* writeKey = "eca583f868caeea9";
-
-//Adafruit_Si7021 sensor = Adafruit_Si7021();
+unsigned int channelId = 77248;
+const char* writeKey = "baf4da406f9766b8";
 
 void setup(void){
   M5.begin();
+  M5.Power.begin();
+  M5.Lcd.setTextColor(YELLOW);
+  M5.Lcd.setTextSize(2); 
+  M5.Lcd.setCursor(0, 0); 
   Serial.begin(9600); //温度表示確認用DS18B20用
-  
   sensorsds.setResolution(SENSER_BIT);//温度表示確認用DS18B20用
   pinMode(ONE_WIRE_BUS,INPUT);
- 
   WiFi.begin(ssid,password);
   while(WiFi.status() != WL_CONNECTED){
     delay(500);
     M5.Lcd.print("_");
   }
-  M5.Lcd.print("\r\nWiFi connected\r\nIP address:");
-  M5.Lcd.println(WiFi.localIP());
-  ambient.begin(channelId,writeKey,&client);
-
-  //if(!sensor.begin()){
-   // M5.Lcd.println("Did not find Si7032 sensor");
-   // while(true){
-   //   delay(0);
-   // }
-  //}
-
- 
+  M5.Lcd.printf("\r\nWiFi connected\r\nIP address:");
+  M5.Lcd.printf(WiFi.localIP().toString().c_str());
+  Serial.println("Initializing Ambient...");
+  if (ambient.begin(channelId, writeKey, &client)) 
+  {M5.Lcd.println("Ambient connected successfully");}
+  else
+  {
+  M5.Lcd.println("Failed to connect Ambient");
+  }
 }
 
 void loop(void){
-  //float temp = sensor.readTemperature();
-  //float humid = sensor.readHumidity();
-  //M5.Lcd.printf("temp:%.2f,humid:%2f\r\n",temp,humid);
-
-
   //温度表示確認用DS18B20用(1)
   sensorsds.requestTemperatures();              // 温度取得要求
-  M5.Lcd.println(sensorsds.getTempC(temp1));  //温度1の取得&シリアル送信
-  M5.Lcd.println(sensorsds.getTempC(temp2));  //温度2の取得&シリアル送信
-  M5.Lcd.println(sensorsds.getTempC(temp3));  //温度3の取得&シリアル送信
   
   int t1 = sensorsds.getTempC(temp1);
   int t2 = sensorsds.getTempC(temp2);
   int t3 = sensorsds.getTempC(temp3);
 
-    
+  //M5.Lcd.println(sensorsds.getTempC(temp1));  //温度1の取得&シリアル送信
+  //M5.Lcd.println(sensorsds.getTempC(temp2));  //温度2の取得&シリアル送信
+  //M5.Lcd.println(sensorsds.getTempC(temp3));  //温度3の取得&シリアル送信
+
  // ambient.set(1,temp);
   ambient.set(1,t1);
   ambient.set(2,t2);
   ambient.set(3,t3);
-  ambient.send();
-  delay(1000*5);
+  bool sendResult = ambient.send();
+  if (sendResult) {
+    M5.Lcd.println("Data sent successfully");
+  } else {
+    M5.Lcd.println("Failed to send data");
+  }
+  delay(1000*60*5);
+  
 }
